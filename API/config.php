92e5496d5c8278abb97a88bd5dde6a97a2600798
bobbyloock <bobby.loock@gmail.com>
2017-05-22 19:06:34 -0600

    made changes to config.php for db user and pass

--- API/config.php
+++ API/config.php
@@ -7,7 +7,7 @@ $baseURI = '/~nb06777/CS4450/v1';
 
 class Config{
 	const HOST = '137.190.19.16,10433';
-	const DBNAME = 'CS4450Spring2017';
-	const USER = 'NathanBrooks';
-	const PASS = '!Bella22';
+	const DBNAME = 'CS4450Fall2017';
+	const USER = 'CS4450Fall2017';
+	const PASS = 'y$0m3thing';
 } 
